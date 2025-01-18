f47c91c876cf1fa624e1fd04b1cc1875a52ae94a
Angel Andreev <angelandreev1996@gmail.com>
2025-01-18 21:31:01 +0200

    Update merge_sort.py

--- Searching&SortingAlgorithms/merge_sort.py
+++ Searching&SortingAlgorithms/merge_sort.py
@@ -2,22 +2,31 @@ numbers = [int(x) for x in input().split()]
 
 # Memory: O(n*log(n))
 def merge_sort(nums):
-if len(nums) == 1:
-return nums
-mid_idx = len(nums) // 2
-left = nums[:mid_idx]
-right = nums[mid_idx:]
-return merge_arrays(merge_sort(left), merge_sort(right))
+    if len(nums) == 1:
+        return nums
+    mid_idx = len(nums) // 2
+    left = nums[:mid_idx]
+    right = nums[mid_idx:]
+    return merge_arrays(merge_sort(left), merge_sort(right))
 
 def merge_arrays(left, right):
-sorted_arr = []
-left_idx, right_idx = 0, 0
-while left_idx < len(left) and right_idx < len(right):
-if left[left_idx] < right[right_idx]:
-sorted_arr.append(left[left_idx])
-left_idx += 1
-else:
-sorted_arr.append(right[right_idx])
-right_idx += 1
-# TODO: Take remaining elements either from the left or right
-return sorted_arr
+    sorted_arr = []
+    left_idx, right_idx = 0, 0
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            sorted_arr.append(left[left_idx])
+            left_idx += 1
+        else:
+            sorted_arr.append(right[right_idx])
+            right_idx += 1
+    # Add any remaining elements from left or right
+    while left_idx < len(left):
+        sorted_arr.append(left[left_idx])
+        left_idx += 1
+    while right_idx < len(right):
+        sorted_arr.append(right[right_idx])
+        right_idx += 1
+    return sorted_arr
+
+sorted_numbers = merge_sort(numbers)
+print(*sorted_numbers)
